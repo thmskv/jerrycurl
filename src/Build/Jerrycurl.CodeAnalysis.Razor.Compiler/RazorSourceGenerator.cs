e9df49f3f8b5e56843f8eecb978b941121df32b4
Thomas Skov <thomas@dancode.net>
2024-10-11 16:17:11 +0200

    test language version detection

--- src/Build/Jerrycurl.CodeAnalysis.Razor.Compiler/RazorSourceGenerator.cs
+++ src/Build/Jerrycurl.CodeAnalysis.Razor.Compiler/RazorSourceGenerator.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Net;
 using System.Xml.Serialization;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Jerrycurl.CodeAnalysis.Razor.Compiler;
 
@@ -24,6 +25,13 @@ public class RazorSourceGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+        var options = context.ParseOptionsProvider.Select((opt, _) => opt as CSharpParseOptions);
+
+        context.RegisterSourceOutput(options, (context, data) =>
+        {
+            context.AddSource("_ParseOptions", "/*" + data.Language + " " + data.LanguageVersion + " " + data.SpecifiedLanguageVersion + "*/");
+        });
+
         var sourceItems = context.AdditionalTextsProvider
             .Where(static file => file.Path.EndsWith(".cssql", StringComparison.OrdinalIgnoreCase))
             .Select(static (file, cancellationToken) =>
@@ -55,8 +63,6 @@ public class RazorSourceGenerator : IIncrementalGenerator
             finalItems,
             (context, data) =>
             {
-                //Debugger.Launch();
-
                 var additionalText = data.Left.Left;
                 var config = data.Left.Right;
                 var imports = data.Right;
@@ -78,10 +84,9 @@ public class RazorSourceGenerator : IIncrementalGenerator
 
                 void Go()
                 {
-
-
                     config.GlobalOptions.TryGetValue("build_property.ProjectDir", out var projectDirectory);
                     config.GlobalOptions.TryGetValue("build_property.RootNamespace", out var rootNamespace);
+                    config.GlobalOptions.TryGetValue("build_property.LangVersion", out var rootNamespace);
 
                     var project = new RazorProject()
                     {
